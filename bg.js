8ca4d67050f483e0224d1e075fb80a8375fb0466
Shrihari Mohan <shrihari.uma.1999@gmail.com>
2024-04-05 22:16:25 +0530

    Add v 1.0.2 - Extension changes

--- bg.js
+++ bg.js
@@ -1,41 +1,23 @@
 const generatePreview = async (tabId) => {
   console.log("🔥 ~ generatePreview :")
-  chrome.scripting.executeScript({
+  await chrome.scripting.executeScript({
     files: ["popup.js"],
     target: { tabId },
   })
-    .then(() => console.log("script injected"));
-
-  chrome.action.setBadgeBackgroundColor({
-    color: "#0e7490",
-    tabId
-  });
-}
-
-const changeActionItem = async (tabId) => {
-  console.log("🔥 ~ changeActionItem :")
-  chrome.action.setBadgeBackgroundColor({
-    color: "#171717",
-    tabId
-  });
+  console.log("script injected")
 }
 
 
 chrome.action.onClicked.addListener(async (tab) => {
-  const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
-  const nextState = prevState === 'ON' ? 'OFF' : 'ON'
-  if (nextState == 'ON') {
-    await generatePreview(tab.id)
-  }
-  else {
-    await changeActionItem(tab.id)
-  }
-  chrome.action.setBadgeText({ text: nextState, tabId: tab.id });
+  chrome.action.setBadgeText({ text: "WAIT", tabId: tab.id });
+  await generatePreview(tab.id)
+  chrome.action.setBadgeText({ text: "ON", tabId: tab.id });
+
 });
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.action.setBadgeText({
-    text: "OFF"
+    text: "ON"
   });
 
   chrome.action.setBadgeBackgroundColor({
